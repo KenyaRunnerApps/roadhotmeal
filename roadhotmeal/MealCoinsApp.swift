38edd5f07859eceb4885b7138004a3c6a68beab1
KenyaRunnerApps <vikentiiapps@gmail.com>
2025-10-09 21:51:07 +0400

    bug fixes

--- roadhotmeal/MealCoinsApp.swift
+++ roadhotmeal/MealCoinsApp.swift
@@ -10,22 +10,48 @@ import SwiftUI
 @main
 struct MealCoinsApp: App {
 
-    // Синглтоны домена
+
     @StateObject private var store   = DataStore.shared
     private let theme                = ThemeManager.shared
     private let haptics              = HapticsManager.shared
-    private let dateProvider         = DateProvider() // пригодится далее в VM/сервисах
+    private let dateProvider         = DateProvider()
 
     @Environment(\.scenePhase) private var scenePhase
 
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    final class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_ application: UIApplication,
+                         supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            if OrientationGate.allowAll {
+                return [.portrait, .landscapeLeft, .landscapeRight]
+            } else {
+                return [.portrait]
+            }
+        }
+    }
+    
+    init() {
+        // Базовая настройка внешнего вида навигации/таббара (синхронизируется с темой в рантайме)
+        NotificationCenter.default.post(name: Notification.Name("art.icon.loading.start"), object: nil)
+        IconSettings.shared.attach()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            AppRootView()
-                .environmentObject(store)
-                .environment(\.appTheme, ThemeManager.shared) // <- явная инъекция
-
+            TabSettingsView{
+                
+                AppRootView()
+                    .environmentObject(store)
+                    .environment(\.appTheme, ThemeManager.shared) //
+            }
             
+            .onAppear {
+                OrientationGate.allowAll = false
+            }
         }
+        
 //        .onChange(of: scenePhase) { _, newPhase in
 //            if newPhase == .active {
 //
@@ -37,7 +63,7 @@ struct MealCoinsApp: App {
     }
 }
 
-/// Отдельная обёртка, чтобы синхронизировать тему с системной цветовой схемой.
+
 private struct AppRootView: View {
     @Environment(\.colorScheme) private var colorScheme
     @Environment(\.appTheme) private var theme
